bd752cc60394f29d8ddfa8363f52636af3c71578
KonstantinKomarov <konstakom2605@mail.ru>
2026-03-31 20:17:55 +0300

    Исправил

--- 2 задание/2 задание/main.cpp
+++ 2 задание/2 задание/main.cpp
@@ -1,40 +1,44 @@
-﻿#include <iostream>
+#include <iostream>
 #include <deque>
+#include <iterator>
 
 int main() {
     std::deque<int> D;
-    int N;
-
-    std::cout << "Enter number of elements (even): ";
-    std::cin >> N;
-
-    if (N % 2 != 0) {
+    
+    std::cout << "Enter elements (even number of elements): ";
+    
+    // Читаем все числа из потока ввода до конца
+    std::copy(std::istream_iterator<int>(std::cin),
+              std::istream_iterator<int>(),
+              std::back_inserter(D));
+    
+    // Проверяем, что количество элементов четное
+    if (D.size() % 2 != 0) {
         std::cout << "Number of elements must be even!" << std::endl;
         return 1;
     }
-
-    std::cout << "Enter " << N << " elements: ";
-    for (int i = 0; i < N; ++i) {
-        int value;
-        std::cin >> value;
-        D.push_back(value);
+    
+    if (D.empty()) {
+        std::cout << "No elements entered!" << std::endl;
+        return 1;
     }
-
+    
+    int N = D.size();
     auto i = D.begin();
     std::advance(i, N / 2);
-
+    
     for (int k = 0; k < N / 2; ++k) {
         i = D.insert(i, -1);
-
+        
         ++i;
         ++i;
     }
-
+    
     std::cout << "Resulting deque: ";
     for (int val : D) {
         std::cout << val << " ";
     }
     std::cout << std::endl;
-
+    
     return 0;
 }
